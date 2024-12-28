19f1fc0adb6371def2d4e709f0d911fee0c154f2
Pranav Pahuja <20995301+pranavpahuja@users.noreply.github.com>
2024-12-28 20:07:14 +0530

    Create fresh.sh

---
+++ scripts/fresh.sh
@@ -0,0 +1,35 @@
+echo "Welcome Boss!"
+echo "Let's go ahead and setup your mac!"
+
+echo "STEP 1 - Let me check for Homebrew and install if we don't have it..."
+which -s brew
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
+
+echo "STEP 2 - Let me check for ZSH now..."
+
+if ! command -v zsh >/dev/null 2>&1; then
+    echo "ZSH is not installed. Installing ZSH..."
+    brew install zsh
+else
+    echo "ZSH is already installed."
+fi
+
+echo "Step 3 - Checking for Oh My Zsh and install if we don't have it"
+if test ! $(which omz); then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "OMZ is installed"
+fi
+
+echo "Step 4 - Let me check and install some brew formulae..."
+echo "1 -> aerospace"
+brew install --cask nikitabobko/tap/aerospace
+
+echo "2 -> maccy"
+brew install maccy
+
+echo "3 -> keyclu"
+brew install --cask keyclu
