1522de422a14a3b87fd51b6b59e1286839b49a5b
Pranav Pahuja <20995301+pranavpahuja@users.noreply.github.com>
2025-02-09 03:55:54 +0530

    Update fresh.sh

--- scripts/fresh.sh
+++ scripts/fresh.sh
@@ -23,20 +23,3 @@ if test ! $(which omz); then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     echo "OMZ is installed"
 fi
-
-echo "Step 4 - Let me check and install some brew formulae..."
-echo "1 -> aerospace"
-brew install --cask nikitabobko/tap/aerospace
-
-echo "2 -> maccy"
-brew install maccy
-
-echo "3 -> keyclu"
-brew install --cask keyclu
-
-echo "4 -> fortune and cowsay"
-brew install fortune
-brew install cowsay
-
-echo "5 -> ghostty"
-brew install --cask ghostty
